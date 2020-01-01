1f0201a0f603cbb282ba4e343070f859133b3c50
kalelman <erick_rojas_perez@hotmail.com>
2020-01-01 17:07:34 -0600

    RecyclerView Basic Funcionallity Implementation

--- app/src/main/java/com/kalelman/recyclerview_kotlin/MainActivity.kt
+++ app/src/main/java/com/kalelman/recyclerview_kotlin/MainActivity.kt
@@ -2,11 +2,22 @@ package com.kalelman.recyclerview_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kalelman.recyclerview_kotlin.adapter.ListSelectionRecyclerViewAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var listsRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listsRecyclerView = findViewById(R.id.list_recycler_view)
+
+        listsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
     }
 }
---
+++ app/src/main/java/com/kalelman/recyclerview_kotlin/adapter/ListSelectionRecyclerViewAdapter.kt
@@ -0,0 +1,26 @@
+package com.kalelman.recyclerview_kotlin.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kalelman.recyclerview_kotlin.R
+
+class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+    private val listTitles = arrayOf("Shopping list", "Chores", "Android Tutorials")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder, parent, false)
+
+        return ListSelectionViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listTitles.size
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        holder.listPosition.text = (position + 1).toString()
+        holder.listTitle.text = listTitles[position]
+    }
+}

2733c693e6c9a347777b43735224dd09ecf6d79b
kalelman <erick_rojas_perez@hotmail.com>
2020-01-01 18:25:08 -0600

    Adding more items for the RecyclerVieew

--- app/src/main/java/com/kalelman/recyclerview_kotlin/adapter/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/kalelman/recyclerview_kotlin/adapter/ListSelectionRecyclerViewAdapter.kt
@@ -7,7 +7,10 @@ import com.kalelman.recyclerview_kotlin.R
 
 class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>() {
 
-    private val listTitles = arrayOf("Shopping list", "Chores", "Android Tutorials")
+    private val listTitles = arrayOf("Shopping list", "Chores", "Android Tutorials"
+        , "Python Tutorials", "C++ Tutorials", "Java Tutorials", "Kotlin Tutorials", "C++ Tutorials"
+        , "Swift Tutorials", "iOS Tutorials", "Machine Learning Tutorials", "JavaScript Tutorials"
+        , "CSS Tutorials" , "HTML Tutorials")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder, parent, false)
